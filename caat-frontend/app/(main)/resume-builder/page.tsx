265b9b9f359c84a5704e4b324c9ba4361795aec6
Jaden Khuu <jaden.khuu@gmail.com>
2025-12-26 18:50:36 +1100

    added top bar to resume, placeholder text for sidebar profile

--- caat-frontend/app/(main)/resume-builder/page.tsx
+++ caat-frontend/app/(main)/resume-builder/page.tsx
@@ -1,5 +1,30 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import ResumeBuilderClient from "./client";
+import { 
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
 
 export default function ResumeBuilderPage() {
-  return <ResumeBuilderClient />;
+  return <>
+    <header className="flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <SidebarTrigger className="-ml-1" />
+        <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+        />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink>Resume Builder</BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+      
+    <ResumeBuilderClient />;
+  </>
 }
--- caat-frontend/components/nav-user.tsx
+++ caat-frontend/components/nav-user.tsx
@@ -70,7 +70,7 @@ export function NavUser({
             align="end"
             sideOffset={4}
           >
-            <DropdownMenuLabel className="p-0 font-normal">
+            {/* <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.name} />
@@ -83,18 +83,19 @@ export function NavUser({
                   </span>
                 </div>
               </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
+            </DropdownMenuLabel> */}
+            {/* <DropdownMenuSeparator /> */}
             <DropdownMenuGroup>
                 
-              <DropdownMenuItem>
+              {/* <DropdownMenuItem>
                 <IconUserCircle />
                 Account
               </DropdownMenuItem>
               <DropdownMenuItem>
                 <IconCreditCard />
                 Billing
-              </DropdownMenuItem>
+              </DropdownMenuItem> */}
+
               <DropdownMenuItem>
                 <IconNotification />
                 Notifications
